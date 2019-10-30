f7b1bfce24342344957e58daac5f52d087fe9dfc
Med-Anis <56160030+Med-Anis@users.noreply.github.com>
2019-10-30 17:15:56 +0100

    Update Program.cs

--- Profil_Projekt_1/Profil_Projekt_1/Program.cs
+++ Profil_Projekt_1/Profil_Projekt_1/Program.cs
@@ -7,38 +7,104 @@ namespace ExamplePrg
     {
 
         static void Main(string[] args)
-        { //Eingabe
-          // Eingabe Länge 
-            Console.WriteLine("Länger eingeben:");
-            String eingabe = Console.ReadLine();
-            double zahl = Convert.ToDouble(eingabe);
+        { 
+            
+            
+            //Eingabe
+
+             
+             
+              // Eingabe Länge 
+              Console.WriteLine("Länger eingeben:");
+              String eingabe = Console.ReadLine();
+              double zahl = Convert.ToDouble(eingabe);
 
 
-            // Eingabe Breite
-            Console.WriteLine("Breite eingeben:");
-            String eingabe2 = Console.ReadLine();
-            double zahl2 = Convert.ToDouble(eingabe);
+              // Eingabe Breite
+              Console.WriteLine("Breite eingeben:");
+              String eingabe2 = Console.ReadLine();
+              double zahl2 = Convert.ToDouble(eingabe);
+ 
+ 
+              // Eingabe Tiefe
+              Console.WriteLine("Tiefe eingeben:");
+              String eingabe3 = Console.ReadLine();
+              double zahl3 = Convert.ToDouble(eingabe);
 
 
-            // Eingabe Tiefe
-            Console.WriteLine("Tiefe eingeben:");
-            String eingabe3 = Console.ReadLine();
-            double zahl3 = Convert.ToDouble(eingabe);
+              //Eingabe Dicke 
+              Console.WriteLine("Dicke eingeben:");
+              String eingabe4 = Console.ReadLine();
+              double zahl4 = Convert.ToDouble(eingabe);
+
+        
+
+            // Eingabe Dichte
+            Console.WriteLine("Bitte geben sie die dichte des Profils");
+            Console.ReadLine(D);
+
 
 
             //Querschnittfläche berechnung
-            Querschnittfläche = zahl1 * zahl2 * zahl3
+
+            Console.WriteLine("Eigenschaften eingeben (L= 0 oder 1)");
+            Console.ReadLine(L)
+
+
+          
+
+
+            while (L = 0) || (L = 1 );
+            {
+                
+            
+                if (L = 1) ;
+                {
+                    Querschnittfläche = (zahl1 * zahl2 * zahl3) - ((zahl1 - zahl4) * (zahl2 - zahl4));
+                    Volume = (zahl1 * zahl2 * zahl3) - ((zahl1 - zahl4) * (zahl2 - zahl4) * zahl3);
+                }
 
 
 
-                // Initialisieren der Variablen
+
+               else
+               { 
+                        Querschnittfläche2 = (zahl1 * zahl2 );
+                        Volume2 = (zahl1 * zahl2 * zahl3);  
+                }
+
+
+
+                Gewuicht = Volume * Dichte;
+
+
+
+
+
+
+
+            }
+
+           //
+
+
+
+
+
+
+
+            // Initialisieren der Variablen
             double ergebnis;
             double divisor = 3;
+            double Querschnittfläche = 0;
+            int L ;
 
 
 
-            // Arithmetik
-            ergebnis = zahl + zahl;
+
+
+             // Arithmetik
+             ergebnis = zahl + zahl;
             ergebnis = zahl * 2;
             ergebnis = (zahl + zahl) * 2;
             ergebnis = Math.Pow(zahl, 2);
